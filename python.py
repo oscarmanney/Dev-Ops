3d2305a97639b731221cf83c4b1a2e8f4717ab67
oscarmanney <114562467+oscarmanney@users.noreply.github.com>
2025-10-26 01:34:48 +0100

    Updateed python.py with some python script

---
+++ python.py
@@ -0,0 +1,10 @@
+import smtplib
+#SERVER = "localhost"
+
+FROM = 'monty@python.com'
+
+TO = ["jon@mycompany.com"] # must be a list
+
+SUBJECT = "Hello!"
+
+TEXT = "This message was sent with Python's smtplib."
